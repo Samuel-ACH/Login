d2d9ea5811d73f1bd46e6cec214e4a3e8f34ed35
AnthoniAvila-UNAH <anthoni.avila@unah.hn>
2024-02-09 21:33:50 -0600

    Cambios PIN

--- PHP/Vistas/Pin.php
+++ PHP/Vistas/Pin.php
@@ -8,19 +8,23 @@
     <title>Solicitud de Pin de Seguridad</title>
 </head>
 <body>
-    <form method="POST">
-        <h2>Pin de Seguridad</h2>
-        <p>Ingresa un nuevo Pin</p> 
+    <main>
+        <div>
+            <form method="POST">
+                <h2>Pin de Seguridad</h2>
+                <p>Ingresa un nuevo Pin</p> 
 
-        <div class="input-wrapper">
-            <input type="int" name="pin" placeholder="Ingresa tu pin de seguridad">
-            <img class="input-icon" src="../../Imagenes/password.svg" alt="">
+                <div class="input-wrapper">
+                    <input type="int" name="pin" placeholder="Ingresa tu pin de seguridad">
+                    <img class="input-icon" src="../../Imagenes/password.svg" alt="">
+                </div>
+                <div class="input-wrapper">
+                    <input type="int" name="pin" placeholder="Ingresa nuevamente tu Pin">
+                    <img class="input-icon" src="../../Imagenes/password.svg" alt="">
+                </div>
+                <input class="btn" type="submit" name="register" value="Enviar">
+            </form>
         </div>
-        <div class="input-wrapper">
-            <input type="int" name="pin" placeholder="Ingresa nuevamente tu Pin">
-            <img class="input-icon" src="../../Imagenes/password.svg" alt="">
-        </div>
-        <input class="btn" type="submit" name="register" value="Enviar">
-    </form>
+    </main>
 </body>
 </html>
