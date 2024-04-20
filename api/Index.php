898e16c50c9910b7ac27d083e1a1c29e3a35ac78
Samuel-ACH <98471427+Samuel-ACH@users.noreply.github.com>
2024-04-20 00:33:40 -0600

    test

--- api/Index.php
+++ api/Index.php
@@ -5,7 +5,7 @@ session_start();
 if (isset($_SESSION["correo"])) {
     header("location: Main.php");https://github.com/Samuel-ACH/Tech-Titans-RED/blob/main/PHP/Vistas/Index.php
 }
-include('../Controladores/Conexion/Conexion_be.php');
+include('../PHP/Controladores/Conexion/Conexion_be.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -16,12 +16,12 @@ include('../Controladores/Conexion/Conexion_be.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Clínica RED</title>
-    <link rel="shortcut icon" href="../../EstilosLogin/images/pestana.png" type="image/x-icon">
+    <link rel="shortcut icon" href="./EstilosLogin/images/pestana.png" type="image/x-icon">
 
     <link
         href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
         rel="stylesheet">
-    <link rel="stylesheet" href="../../EstilosLogin/css/estilos.css">
+    <link rel="stylesheet" href="./EstilosLogin/css/estilos.css">
 
    <!-- <script src="https://www.google.com/recaptcha/api.js" async defer></script> -->
 
@@ -46,8 +46,8 @@ include('../Controladores/Conexion/Conexion_be.php');
             <!--Formulario de Login y registro-->
             <div class="contenedor__login-register" id="formulario">
                 <!--Login-->
-                <form action="../Controladores/LoginController_be.php" method="POST" class="formulario__login"id="loginForm" >
-                    <img src="../../EstilosLogin/images/logo.png" alt="Logo" class="logo">
+                <form action="../PHP/Controladores/LoginController_be.php" method="POST" class="formulario__login"id="loginForm" >
+                    <img src="./EstilosLogin/images/logo.png" alt="Logo" class="logo">
                     <h2>Iniciar Sesión</h2>
                     <!-- Grupo: Correo -->
                     <div class="formulario__grupo" id="grupo__correo">
@@ -103,12 +103,12 @@ include('../Controladores/Conexion/Conexion_be.php');
                     </div>
 
                     <div>  
-                    <a href="../Seguridad/OTP/V_Recuperar_Clave/V_ingresar_correo.php">¿No recuerdas tu contraseña?</a>
+                    <a href="../PHP/Seguridad/OTP/V_Recuperar_Clave/V_ingresar_correo.php">¿No recuerdas tu contraseña?</a>
                     </div>
                 </form>
 
                 <!--Register-->
-                <form action="../Controladores/Registro_Usuario_be.php" method="POST" class="formulario__register" id="registerForm">
+                <form action="../PHP/Controladores/Registro_Usuario_be.php" method="POST" class="formulario__register" id="registerForm">
                     <h2>Regístrate</h2>
                     <!-- GRUPO DNI -->
                     <div class="formulario__grupo" id="grupo__dni">
@@ -222,13 +222,13 @@ include('../Controladores/Conexion/Conexion_be.php');
             </div>
         </div>
     </main>
-    <script src="../../EstilosLogin/js/script.js"></script>
+    <script src="../EstilosLogin/js/script.js"></script>
     <script src="https://kit.fontawesome.com/2c36e9b7b1.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
     <!-- <script type="module" src="../javascript/validacionIndex.js"></script> -->
     <script type="module" src="../javascript/validacionAutoRegistro.js"></script>
-    <script src="../javascript/captcha.js"></script>
+    <script src="../PHP/javascript/captcha.js"></script>
 </body>
 </html>
