14ddc777af19bd06f76a29e1bcf638701daf9d67
AnthoniAvila-UNAH <158218989+AnthoniMatute24@users.noreply.github.com>
2024-04-26 14:31:53 -0600

    Update C_estado_finalizado_cita.php

--- PHP/Negocio/Procesos/C_procesos/C_estado_finalizado_cita.php
+++ PHP/Negocio/Procesos/C_procesos/C_estado_finalizado_cita.php
@@ -7,30 +7,35 @@ if (session_status() === PHP_SESSION_ACTIVE) {
     session_start();
 }
 
-include('/PHP/Controladores/bitacora.php');
 // Verificar si se ha confirmado el envío
 if (isset($_POST['guardarDatos'])) {
     // Realizar la actualización del estado de la cita en la base de datos
-    include '/PHP/Controladores/Conexion/Conexion_be.php';
-    
+    include '../../../Controladores/Conexion/Conexion_be.php';
+    include('../../../Controladores/bitacora.php');
 
-    // Obtener el ID de la cita
-    $idCita = $_POST['Id_Cita_U'];
+    if (isset($_SESSION['datosPaciente'])) {
+        // Obtener los datos del paciente de las variables de sesión
+        $datosArray = $_SESSION['datosPaciente'];
 
-    // Actualizar el estado de la cita a través de una consulta SQL
-    $sql = "UPDATE tbl_cita_terapeutica SET Id_Estado_Cita = 4 WHERE id_Cita_Terapia = '$idCita'";
+        // Obtener el ID de la cita
+        $idCita = $datosArray[0];
 
-    if (mysqli_query($conexion, $sql)) {
-        $n=$_SESSION['id_D'];          //obtiene valor de la variable sesion
-        $a='FINALIZAR';
-        $d="LA CITA CON ID ". $idCita ." SE HA FINALIZADO.";
-        bitacora($n, $a, $d);
-        // echo "El estado de la cita se ha actualizado correctamente.";
-        header("Location: ../../../Vistas/Main.php");
-    } else {
-        echo "Error al actualizar el estado de la cita: " . mysqli_error($conexion);
+        // Actualizar el estado de la cita a través de una consulta SQL
+        $sql = "UPDATE tbl_cita_terapeutica SET Id_Estado_Cita = 4 WHERE id_Cita_Terapia = '$idCita'";
+
+        if (mysqli_query($conexion, $sql)) {
+            // echo "El estado de la cita se ha actualizado correctamente.";
+            header("Location: ../../../Vistas/Main.php");
+            $n = $_SESSION['id_D'];          //obtiene valor de la variable sesion
+            $a = 'FINALIZAR';
+            $d = "LA CITA CON ID " . $idCita . " SE HA FINALIZADO.";
+            bitacora($n, $a, $d);
+        } else {
+            echo "Error al actualizar el estado de la cita: " . mysqli_error($conexion);
+        }
+        // Cerrar la conexión
+        mysqli_close($conexion);
+        // Limpiar las variables de sesión después de utilizar los datos
+        unset($_SESSION['datosPaciente']);
     }
-    // Cerrar la conexión
-    mysqli_close($conexion);
 }
-?>
