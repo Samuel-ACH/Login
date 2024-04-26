b6f8f3d87fd14566df0d8afd8fa11d19f8081c4c
AnthoniAvila-UNAH <158218989+AnthoniMatute24@users.noreply.github.com>
2024-04-26 17:12:11 -0600

    Update V_editar_permisos.php

--- PHP/Seguridad/Roles_permisos/permisos/V_editar_permisos.php
+++ PHP/Seguridad/Roles_permisos/permisos/V_editar_permisos.php
@@ -13,7 +13,7 @@ include '../../../Controladores/Conexion/Conexion_be.php';
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
     <title>CLÍNICA RED</title>
-    <link rel="shortcut icon" href="../../../../EstilosLogin/images/pestana.png" type="image/x-icon">
+    <link rel="shortcut icon" href="./EstilosLogin/images/pestana.png" type="image/x-icon">
 
     <meta content="" name="description">
     <meta content="" name="keywords">
@@ -143,8 +143,26 @@ include '../../../Controladores/Conexion/Conexion_be.php';
 
     <!-- ----------------CODIGO PARA GENERAR REPORTES------------------>
     <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script> <!-- ESTILOS DE LA TABLA -->
+    <script src="https://cdn.datatables.net/buttons/1.5.6/js/dataTables.buttons.min.js"></script> 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script> 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script> 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script> <!-- LIBRERIA PDF -->
     <script src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.html5.min.js"></script> <!-- LIBRERIA HTML -->
     <script src="https://kit.fontawesome.com/2c36e9b7b1.js" crossorigin="anonymous"> </script> <!-- ICONOS -->
+
+    <?php
+    // // Ruta de la imagen
+    // $ruta_imagen = '../../../../Imagenes/logo3.jpeg';
+
+    // // Verificar si el archivo existe
+    // if (file_exists($ruta_imagen)) {
+    //     // Leer el contenido de la imagen
+    //     $contenido_imagen = file_get_contents($ruta_imagen);
+
+    //     // Codificar la imagen en base64
+    //     $ImagenBase64 = base64_encode($contenido_imagen);
+    // }
+    ?>
 
     <script>
         // REPORTE DE PARAMETROS 
