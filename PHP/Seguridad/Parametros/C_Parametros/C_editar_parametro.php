571f774d128e2ea7509870577b6c6c14783f27a1
AnthoniAvila-UNAH <158218989+AnthoniMatute24@users.noreply.github.com>
2024-04-27 16:23:55 -0600

    comentario de bitacora (parametros)

--- PHP/Seguridad/Parametros/C_Parametros/C_editar_parametro.php
+++ PHP/Seguridad/Parametros/C_Parametros/C_editar_parametro.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 include('../../../Controladores/Conexion/Conexion_be.php');
-include('../../../Controladores/bitacora.php');
+// include('../../../Controladores/bitacora.php');
 
 $Id_Parametro = $_POST['Id_Parametro'];
 $valorParametroE = $_POST['valorParametroE'];
@@ -10,8 +10,8 @@ $sql = "UPDATE tbl_ms_parametros SET Valor = '$valorParametroE', Modificado_Por 
         WHERE Id_Parametro = '$Id_Parametro'";
 
     echo $resultado = mysqli_query($conexion, $sql);
-    $n=$_SESSION['id_D'];          //obtiene valor de la variable sesion
-    $a='EDITAR';
-    $d="SE HA EDITADO EL PARAMETRO CON EL ID ". $Id_Parametro.".";
-    bitacora($n, $a, $d);
+    // $n=$_SESSION['id_D'];          //obtiene valor de la variable sesion
+    // $a='EDITAR';
+    // $d="SE HA EDITADO EL PARAMETRO CON EL ID ". $Id_Parametro.".";
+    // bitacora($n, $a, $d);
 ?>
--- PHP/Seguridad/Parametros/C_Parametros/C_eliminar_parametro.php
+++ PHP/Seguridad/Parametros/C_Parametros/C_eliminar_parametro.php
@@ -1,15 +1,15 @@
 <?php
 session_start();
 include('../../../Controladores/Conexion/Conexion_be.php');
-include('../../../Controladores/bitacora.php');
+// include('../../../Controladores/bitacora.php');
 
 $Id_Parametro = $_POST['Id_Parametro'];
 
 $sql = "DELETE FROM tbl_ms_parametros WHERE Id_Parametro = '$Id_Parametro'";
 
     echo $resultado = mysqli_query($conexion, $sql);
-    $n=$_SESSION['id_D'];          //obtiene valor de la variable sesion
-    $a='ELIMINAR';
-    $d="SE HA ELIMINADO EL PARAMETRO CON ID ". $Id_Terapia_E.".";
-    bitacora($n, $a, $d);
+    // $n=$_SESSION['id_D'];          //obtiene valor de la variable sesion
+    // $a='ELIMINAR';
+    // $d="SE HA ELIMINADO EL PARAMETRO CON ID ". $Id_Terapia_E.".";
+    // bitacora($n, $a, $d);
 ?>
