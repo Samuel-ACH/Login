bd46041276fd38b247ae555ee0388467485bad62
AnthoniAvila-UNAH <158218989+AnthoniMatute24@users.noreply.github.com>
2024-04-28 00:52:59 -0600

    Revert "update"
    This reverts commit 0c37dff2935a190e3769ab233471dc4a78f6a8b3.

--- PHP/Negocio/Procesos/C_procesos/C_estado_finalizado_cita_F.php
+++ PHP/Negocio/Procesos/C_procesos/C_estado_finalizado_cita_F.php
@@ -6,27 +6,30 @@ if (session_status() === PHP_SESSION_ACTIVE) {
     // La sesión aún no está iniciada, entonces la inicias
     session_start();
 }
+// include '../../../Controladores/bitacora.php';
 
-// Verificar si se recibió una solicitud POST
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Verificar si se recibió el ID de la cita
-    if (isset($_POST['Id_Cita'])) {
-        // Obtener el ID de la cita enviado desde el cliente
-        $idCita = $_POST['Id_Cita'];
+// Verificar si se ha confirmado el envío
+if (isset($_POST['guardarDatos'])) {
+    // Realizar la actualización del estado de la cita en la base de datos
+    include '../../../Controladores/Conexion/Conexion_be.php';
+    
+    // Obtener el ID de la cita
+    $idCita = $_POST['Id_Cita_U'];
 
-        // Actualizar el estado de la cita a través de una consulta SQL
-        $sql = "UPDATE tbl_cita_terapeutica SET Id_Estado_Cita = 4 WHERE id_Cita_Terapia = '$idCita'";
+    // Actualizar el estado de la cita a través de una consulta SQL
+    $sql = "UPDATE tbl_cita_terapeutica SET Id_Estado_Cita = 4 WHERE id_Cita_Terapia = '$idCita'";
 
-        // Si la actualización fue exitosa, puedes enviar una respuesta al cliente
-        $respuesta = array('success' => true, 'message' => 'Estado de la cita actualizado correctamente');
-        echo json_encode($respuesta);
+    if (mysqli_query($conexion, $sql)) {
+        // $n=$_SESSION['id_D'];          //obtiene valor de la variable sesion
+        // $a='FINALIZAR';
+        // $d="LA CITA CON ID ". $idCita." SE HA FINALIZADO.";
+        // bitacora($n, $a, $d);
+        // echo "El estado de la cita se ha actualizado correctamente.";
+        header("Location: ../../../Vistas/Main.php");
     } else {
-        // Si no se recibió el ID de la cita, enviar una respuesta de error al cliente
-        $respuesta = array('success' => false, 'message' => 'No se recibió el ID de la cita');
-        echo json_encode($respuesta);
+        echo "Error al actualizar el estado de la cita: " . mysqli_error($conexion);
     }
-} else {
-    // Si la solicitud no es de tipo POST, enviar una respuesta de error al cliente
-    $respuesta = array('success' => false, 'message' => 'Solicitud no válida');
-    echo json_encode($respuesta);
+    // Cerrar la conexión
+    mysqli_close($conexion);
 }
+?>
