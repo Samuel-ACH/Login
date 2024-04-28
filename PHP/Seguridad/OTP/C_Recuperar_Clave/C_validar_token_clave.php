87bb4080f5ad9b979747be3652146296e56fac79
Samuel-ACH <98471427+Samuel-ACH@users.noreply.github.com>
2024-04-27 18:46:40 -0600

    fix

--- PHP/Seguridad/OTP/C_Recuperar_Clave/C_validar_token_clave.php
+++ PHP/Seguridad/OTP/C_Recuperar_Clave/C_validar_token_clave.php
@@ -13,12 +13,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['codigo_otp2'])) {
     if ($codigo_otp_ingresado2 == $codigo_otp_correcto2) {
         // El código OTP es correcto, iniciar sesión
         $_SESSION['autenticado'] = true; // Indicar que el usuario está autenticado
-        header("location: ../V_Recuperar_Clave/C_cambiar_clave.php"); // Redirigir al usuario a recuperar la contraseña
+        header("location: ../V_Recuperar_Clave/V_cambiar_clave.php"); // Redirigir al usuario a recuperar la contraseña
         exit();
     } else {
         // El código OTP es incorrecto, almacenar mensaje de error en la sesión
         $_SESSION['error'] = "Código OTP incorrecto. Por favor, inténtalo de nuevo.";
-        header("location: ../V_Recuperar_Clave/C_verificar_token.php"); // Redirigir a la página de verificación de pin
+        header("location: ../V_Recuperar_Clave/V_verificar_token.php"); // Redirigir a la página de verificación de pin
         exit();
     }
 }
