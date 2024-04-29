cc0418d3a70b28a526f28daf17aec2e725a106d4
AnthoniAvila-UNAH <158218989+AnthoniMatute24@users.noreply.github.com>
2024-04-29 14:47:37 -0600

    Update C_procesar_tarjeta.php

--- PHP/Negocio/Expediente/C_Expediente/C_procesar_tarjeta.php
+++ PHP/Negocio/Expediente/C_Expediente/C_procesar_tarjeta.php
@@ -73,7 +73,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             // });
             // </script>
             // ';
-            unset($_SESSION['Id_Detalle_Terapia']);
+            $Id_Detalle_Terapia = '';
         } catch (Exception $e) {
             // Si ocurre algún error, revertir la transacción y mostrar el mensaje de error
             $conexion->rollback();
