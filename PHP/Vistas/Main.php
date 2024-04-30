4b74946ba4f6591bee3bdcfc5290adf8c91a1efe
AnthoniAvila-UNAH <158218989+AnthoniMatute24@users.noreply.github.com>
2024-04-30 16:14:58 -0600

    Update Main.php

--- PHP/Vistas/Main.php
+++ PHP/Vistas/Main.php
@@ -88,8 +88,7 @@ if (!isset($_SESSION["correo"])) {
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"> </script>
     <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.js"></script>
-    <script src="https://cdn.datatables.net/plug-ins/1.10.24/i18n/Spanish.json"></script>
-
+    
     <script>
         $(document).ready(function() {
             $('#tablaAgenda').DataTable({
