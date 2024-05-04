d6fc9ee140ace79aa4ec91db4033eae92ba0ba7c
Anthony <anthony.duarte@unah.hn>
2024-05-04 02:44:15 -0600

    Update C_mostrar_tarjetas.js

--- PHP/Negocio/Expediente/C_Expediente/C_mostrar_tarjetas.js
+++ PHP/Negocio/Expediente/C_Expediente/C_mostrar_tarjetas.js
@@ -12,27 +12,19 @@ function mostrarTarjetasYExpedienteTerapeutico(selectedValue) {
   }
 
   var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function () {
+  xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       var tarjetasHTML = this.responseText;
 
       // Verificar si el ID de la tarjeta ya ha sido mostrado
       if (!tarjetasMostradas.includes(selectedValue)) {
         // Determinar en qué columna agregar las tarjetas alternadamente
-        var columnaActual = document.getElementById(
-          "contenedor-tarjetas-columna1"
-        );
-        var tarjetasColumna1 = document.querySelectorAll(
-          "#contenedor-tarjetas-columna1 .card"
-        ).length;
-        var tarjetasColumna2 = document.querySelectorAll(
-          "#contenedor-tarjetas-columna2 .card"
-        ).length;
+        var columnaActual = document.getElementById("contenedor-tarjetas-columna1");
+        var tarjetasColumna1 = document.querySelectorAll("#contenedor-tarjetas-columna1 .card").length;
+        var tarjetasColumna2 = document.querySelectorAll("#contenedor-tarjetas-columna2 .card").length;
 
         if (tarjetasColumna2 < tarjetasColumna1) {
-          columnaActual = document.getElementById(
-            "contenedor-tarjetas-columna2"
-          );
+          columnaActual = document.getElementById("contenedor-tarjetas-columna2");
         }
 
         // Crear un nuevo elemento de tarjeta
@@ -47,15 +39,13 @@ function mostrarTarjetasYExpedienteTerapeutico(selectedValue) {
         // tarjetasMostradas.push(selectedValue);
 
         // Disparar un evento personalizado para indicar que las tarjetas se han mostrado
-        var tarjetasMostradasEvent = new CustomEvent("tarjetasMostradas", {
-          detail: selectedValue,
-        });
+        var tarjetasMostradasEvent = new CustomEvent('tarjetasMostradas', { detail: selectedValue });
         document.dispatchEvent(tarjetasMostradasEvent);
 
         // Agregar evento de clic a las nuevas tarjetas
-        var nuevasTarjetas = columnaActual.querySelectorAll(".card");
-        nuevasTarjetas.forEach(function (tarjeta) {
-          tarjeta.addEventListener("click", function () {
+        var nuevasTarjetas = columnaActual.querySelectorAll('.card');
+        nuevasTarjetas.forEach(function(tarjeta) {
+          tarjeta.addEventListener('click', function() {
             abrirTarjeta(this);
           });
         });
@@ -63,22 +53,17 @@ function mostrarTarjetasYExpedienteTerapeutico(selectedValue) {
     }
   };
 
-  xhttp.open(
-    "POST",
-    "../V_Expediente/V_tarjetas_expediente_terapeutico.php?tratamiento=" +
-      selectedValue,
-    true
-  );
+  xhttp.open("POST", "../V_Expediente/V_tarjetas_expediente_terapeutico.php?tratamiento=" + selectedValue, true);
   xhttp.send();
 }
 
 // Función para abrir una tarjeta
 function abrirTarjeta(tarjeta) {
-  tarjeta.classList.toggle("active");
+  tarjeta.classList.toggle('active');
 }
 
 // Manejar el evento de cambio en el combobox de tratamientos
-document.getElementById("tratamiento").addEventListener("change", function () {
+document.getElementById("tratamiento").addEventListener("change", function() {
   var selectedValue = this.value;
 
   if (selectedValue !== "0") {
@@ -87,83 +72,88 @@ document.getElementById("tratamiento").addEventListener("change", function () {
       mostrarTarjetasYExpedienteTerapeutico(selectedValue);
       tratamientoActual = selectedValue; // Actualizar la variable del tratamiento actual
       this.value = "0";
-      //   tratamientoActual = "0";
-    }
+    //   tratamientoActual = "0";
+    } 
   }
 });
+
+
+
 
 // Función para recopilar y enviar los datos al servidor
 function guardarDatos() {
-  var datosTarjetas = {}; // Objeto para almacenar los datos de las tarjetas
+    var datosTarjetas = {}; // Objeto para almacenar los datos de las tarjetas
 
-  // Recorrer todas las tarjetas y recopilar los datos
-  var tarjetas = document.querySelectorAll(".formulario__input");
-  tarjetas.forEach(function (tarjeta) {
-    var id = tarjeta.id;
-    var valor = tarjeta.value.trim(); // Obtener el valor del campo y eliminar espacios en blanco
-    // Verificar si el input no tiene el atributo 'readonly'
-    if (!tarjeta.hasAttribute("readonly") && valor !== "") {
-      // Verificar si el campo tiene datos y no es de solo lectura
-      datosTarjetas[id] = valor; // Agregar los datos al objeto
+    // Recorrer todas las tarjetas y recopilar los datos
+    var tarjetas = document.querySelectorAll('.formulario__input');
+    tarjetas.forEach(function(tarjeta) {
+        var id = tarjeta.id;
+        var valor = tarjeta.value.trim(); // Obtener el valor del campo y eliminar espacios en blanco
+        // Verificar si el input no tiene el atributo 'readonly'
+        if (!tarjeta.hasAttribute('readonly') && valor !== '') { // Verificar si el campo tiene datos y no es de solo lectura
+            datosTarjetas[id] = valor; // Agregar los datos al objeto
+        }
+    });
+
+    // Verificar si hay datos para enviar al servidor
+    if (Object.keys(datosTarjetas).length === 0) {
+        // Si no hay datos para guardar, mostrar alerta de SweetAlert2
+        Swal.fire({
+            title: 'Atención',
+            text: 'No hay datos para guardar.',
+            icon: 'warning', // Ícono de advertencia
+            confirmButtonText: 'Aceptar' // Texto del botón de confirmación
+        });
+    } else {
+        // Si hay datos para guardar, mostrar alerta de confirmación de SweetAlert2
+        Swal.fire({
+            title: 'Confirmación',
+            text: '¿Estás seguro que deseas guardar los datos?',
+            icon: 'question', // Ícono de pregunta
+            showCancelButton: true, // Mostrar botón de cancelación
+            confirmButtonText: 'Guardar',
+            cancelButtonText: 'Cancelar'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                // Si el usuario confirma, enviar los datos al servidor
+                var datosJSON = JSON.stringify(datosTarjetas);
+
+                var xhr = new XMLHttpRequest();
+                xhr.open('POST', '../C_Expediente/C_procesar_tarjeta.php', true);
+                xhr.setRequestHeader('Content-Type', 'application/json'); // Especificar el tipo de contenido como JSON
+                xhr.onload = function() {
+                    // Manejar la respuesta del servidor aquí si es necesario
+                    console.log(xhr.responseText);
+                    
+                    // Mostrar alerta de éxito después de guardar los datos
+                    Swal.fire({
+                        title: 'Éxito',
+                        text: 'Los datos se han guardado correctamente.',
+                        icon: 'success',
+                        timer: 2000, // Cerrar automáticamente después de 2 segundos
+                        showConfirmButton: false // Ocultar botón de confirmación
+                    }).then(() => {
+                        // Redirigir al usuario a una nueva página después de guardar los datos
+                        window.location.href = '/PHP/Vistas/Main.php';
+                    });
+                };
+                xhr.send(datosJSON);
+            }
+        });
     }
-  });
-
-  // Verificar si hay datos para enviar al servidor
-  if (Object.keys(datosTarjetas).length === 0) {
-    // Si no hay datos para guardar, mostrar alerta de SweetAlert2
-    Swal.fire({
-      title: "Atención",
-      text: "No hay datos para guardar.",
-      icon: "warning", // Ícono de advertencia
-      confirmButtonText: "Aceptar", // Texto del botón de confirmación
-    });
-  } else {
-    // Si hay datos para guardar, mostrar alerta de confirmación de SweetAlert2
-    Swal.fire({
-      title: "Confirmación",
-      text: "¿Estás seguro que deseas guardar los datos?",
-      icon: "question", // Ícono de pregunta
-      showCancelButton: true, // Mostrar botón de cancelación
-      confirmButtonText: "Guardar",
-      cancelButtonText: "Cancelar",
-    }).then((result) => {
-      if (result.isConfirmed) {
-        // Si el usuario confirma, enviar los datos al servidor
-        var datosJSON = JSON.stringify(datosTarjetas);
-
-        var xhr = new XMLHttpRequest();
-        xhr.open("POST", "../C_Expediente/C_procesar_tarjeta.php", true);
-        xhr.setRequestHeader("Content-Type", "application/json"); // Especificar el tipo de contenido como JSON
-        xhr.onload = function () {
-          // Manejar la respuesta del servidor aquí si es necesario
-          console.log(xhr.responseText);
-
-          // Mostrar alerta de éxito después de guardar los datos
-          Swal.fire({
-            title: "Éxito",
-            text: "Los datos se han guardado correctamente.",
-            icon: "success",
-            timer: 2000, // Cerrar automáticamente después de 2 segundos
-            showConfirmButton: false, // Ocultar botón de confirmación
-          }).then(() => {
-            // Redirigir al usuario a una nueva página después de guardar los datos
-            window.location.href = "/PHP/Vistas/Main.php";
-          });
-        };
-        xhr.send(datosJSON);
-      }
-    });
-  }
 }
 
 // Seleccionar el botón de guardar por su ID
-var botonGuardar = document.getElementById("guardarDatos");
+var botonGuardar = document.getElementById('guardarDatos');
 
 // Agregar el evento 'click' al botón de guardar
-botonGuardar.addEventListener("click", function (event) {
-  // Prevenir el comportamiento predeterminado del formulario (si el botón está dentro de un formulario)
-  event.preventDefault();
-
-  // Llamar a la función 'guardarDatos' cuando se haga clic en el botón
-  guardarDatos();
+botonGuardar.addEventListener('click', function(event) {
+    // Prevenir el comportamiento predeterminado del formulario (si el botón está dentro de un formulario)
+    event.preventDefault();
+    
+    // Llamar a la función 'guardarDatos' cuando se haga clic en el botón
+    guardarDatos();
 });
+
+
+  
