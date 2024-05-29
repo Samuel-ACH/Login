1c0267eea79b00ab2f43a99ec9e2f27ba459dc93
Samuel-ACH <98471427+Samuel-ACH@users.noreply.github.com>
2024-05-28 22:32:06 -0600

    comentar la bitacora

--- PHP/Seguridad/Expediente_Clinico/C_EClinico/C_eliminar_EClinico.php
+++ PHP/Seguridad/Expediente_Clinico/C_EClinico/C_eliminar_EClinico.php
@@ -7,15 +7,15 @@ if (session_status() === PHP_SESSION_ACTIVE) {
     session_start();
 }
 include('../../../Controladores/Conexion/Conexion_be.php');
-include('../../../Controladores/bitacora.php');
+// include('../../../Controladores/bitacora.php');
 
 $Id_Resultado_Evaluacion = $_POST['Id_Resultado_Evaluacion'];
 
 $sql = "DELETE FROM tbl_resultado_evaluacion WHERE Id_Resultado_Evaluacion = '$Id_Resultado_Evaluacion'";
 
-    echo $resultado = mysqli_query($conexion, $sql);
-    $n=$_SESSION['id_D'];          //obtiene valor de la variable sesion
-    $a='ELIMINAR';
-    $d="SE HA ELIMINADO EL TIPO DE EXAMEN CON EL ID ". $Id_Resultado_Evaluacion.".";
-    bitacora($n, $a, $d);
+//     echo $resultado = mysqli_query($conexion, $sql);
+//     $n=$_SESSION['id_D'];          //obtiene valor de la variable sesion
+//     $a='ELIMINAR';
+//     $d="SE HA ELIMINADO EL TIPO DE EXAMEN CON EL ID ". $Id_Resultado_Evaluacion.".";
+//     bitacora($n, $a, $d);
 ?>
