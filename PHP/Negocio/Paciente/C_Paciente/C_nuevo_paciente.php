8929bce543f478febb0d3c8c84a322f5275d2039
Dulce-Diaz <dediazg@unah.hn>
2024-07-26 10:07:07 -0600

    Update C_nuevo_paciente.php
    Accion de crear

--- PHP/Negocio/Paciente/C_Paciente/C_nuevo_paciente.php
+++ PHP/Negocio/Paciente/C_Paciente/C_nuevo_paciente.php
@@ -10,6 +10,7 @@ include('../../../../Recursos/SweetAlerts.php');
 // include('../../../Controladores/bitacora.php');
 //crear conexion
 require_once '../../../Controladores/Conexion/Conexion_be.php';
+include '../../../Seguridad/Roles_permisos/permisos/Obtener_Id_Objeto.php';
 
 // Recibir datos del formulario de nuevo paciente
 $nombre = strtoupper($_POST['nombre']);
@@ -21,6 +22,36 @@ $numerodocumento = $_POST['numero_de_documento'];
 $estadopaciente = 1;
 $ocupacionpaciente = strtoupper($_POST['ocupacion']);
 
+
+
+   // obtener el objeto
+   $id_objeto = Obtener_Id_Objeto('V_Paciente');
+   if ($id_objeto === null) {
+       echo "Error: id_objeto es NULL";
+       exit();
+   }
+   $id_objeto = $conexion->real_escape_string($id_objeto);
+   if ($conexion->query("SET @id_objeto = '$id_objeto'") === FALSE) {
+       echo "Error setting id_objeto variable: " . $conexion->error;
+       exit();
+   }
+
+
+// Obtener el ID del usuario actual desde la sesión
+if (isset($_SESSION['id_D'])) {
+   $current_user_id = $_SESSION['id_D'];
+  
+   $current_user_id = mysqli_real_escape_string($conexion, $current_user_id);
+
+   // Establecer la variable de sesión en QL
+   if ($conexion->query("SET @current_user_id = '$current_user_id'") === FALSE) {
+       echo "Error setting session variable: " . $conexion->error;
+       exit();
+   }
+} else {
+   echo "Error: current_user_id es NULL";
+   exit();
+}
 //Insertar nuevo usuario en la base de datos
 $query = "INSERT INTO tbl_paciente (Nombre, Direccion, FechaNacimiento, IdGenero, Id_Tipo_Documento, Numero_Documento, Estado_Paciente, Ocupacion) 
           VALUES ('$nombre', '$direccion', '$fechanacimiento','$idgenero','$idtipodocumento','$numerodocumento', $estadopaciente , '$ocupacionpaciente')";
